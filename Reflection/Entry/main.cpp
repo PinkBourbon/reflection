3b2d53c207beccbc79c8e0da0ed6eec79b7bb6b4
DESKTOP-1SJCNH3\loho <cgyk.piro@gmail.com>
2024-05-07 12:21:27 +0900

    main.cpp 추가

---
+++ Reflection/Entry/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <type_traits>
+
+
+template<class R, class... Args>
+struct function_traits
+{
+	using return_type = R;
+	static constexpr std::size_t arity = sizeof...(Args);
+
+	void PrintType()
+	{
+		std::cout << "Return type: " << typeid(R).name() << std::endl;
+		std::cout << "argument : " << typeid(Args).name << ... << std::endl;
+	}
+};
+
+int main()
+{
+
+}
